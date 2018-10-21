1e04b32279a4af8f61e72414255f34a4fe351d4e
William Moreno <williammorenoprieto@gmail.com>
2018-10-20 23:26:30 -0500

    V 10.20.2
    User Location

--- app/src/main/java/com/example/willi/appmap/MainActivity.java
+++ app/src/main/java/com/example/willi/appmap/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.willi.appmap;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button GPSbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GPSbutton = findViewById(R.id.button);
+
+        GPSbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/willi/appmap/MapActivity.java
+++ app/src/main/java/com/example/willi/appmap/MapActivity.java
@@ -1,27 +1,230 @@
 package com.example.willi.appmap;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONArray;
+
+import java.util.List;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    private final static int LOCATION_PERMISSION = 0;
+    private static final double RADIUS_OF_EARTH_KM = 6371;
+    private static final double LATAEROPUERTO = 4.697730480112796;
+    private static final double LONGAEROPUERTO = -74.14075972845345;
+    private static int voy;
+
+    private Marker oldmark;
+    private Marker newmark;
+
+
+    //UNA SOLA VEZ
+    private FusedLocationProviderClient mFusedLocationClient;
+    //ACTUALIZACION PERIODICA
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private List<String> listalocations;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        voy=0;
+
+        mLocationRequest = createLocationRequest();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        requestPermission(this, Manifest.permission.ACCESS_FINE_LOCATION,
+                "Se necesita acceder a la ubicacion", LOCATION_PERMISSION);
+
+        LocationSettingsRequest.Builder builder = new
+                LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                startLocationUpdates(); //Todas las condiciones para recibir localizaciones
+            }
+        });
+
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode) {
+                    case CommonStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed by showing the user a dialog.
+                        try {// Show the dialog by calling startResolutionForResult(), and check the result in onActivityResult().
+                            ResolvableApiException resolvable = (ResolvableApiException) e;
+                            resolvable.startResolutionForResult(MapActivity.this ,
+                                    LOCATION_PERMISSION);
+                        } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                        } break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                    // Location settings are not satisfied. No way to fix the settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+
+        mLocationCallback = new LocationCallback() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                Location location = locationResult.getLastLocation();
+                Log.i("LOCATION", "Location update in the callback: " + location);
+                if (location != null && mMap != null) {
+                    if (voy==0)
+                    {
+                        LatLng user = new LatLng(location.getLatitude() , location.getLongitude());
+                        oldmark=mMap.addMarker(new MarkerOptions().position(user).title("Usted").icon(BitmapDescriptorFactory.fromResource(R.drawable.userpin32)));
+                        newmark=oldmark;
+                        mMap.moveCamera(CameraUpdateFactory.zoomTo(19));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(user));
+                        voy++;
+                    }
+                    else
+                    {
+                        LatLng user = new LatLng(location.getLatitude() , location.getLongitude());
+                        oldmark.remove();
+                        newmark=mMap.addMarker(new MarkerOptions().position(user).title("Usted").icon(BitmapDescriptorFactory.fromResource(R.drawable.userpin32)));
+                        oldmark=newmark;
+                        //mMap.moveCamera(CameraUpdateFactory.zoomTo(19));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(user));
+                    }
+                }
+            }
+        };
+    }
+
+    protected LocationRequest createLocationRequest() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000); //tasa de refresco en milisegundos
+        mLocationRequest.setFastestInterval(5000); //máxima tasa de refresco
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return mLocationRequest;
+    }
+
+    private void requestPermission(Activity context, String permission, String explanation, int requestId) {
+        if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context, permission)) {
+                Toast.makeText(context, explanation, Toast.LENGTH_LONG).show();
+            }
+            ActivityCompat.requestPermissions(context, new String[]{permission}, requestId);
+        }
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION: {
+                if (resultCode == RESULT_OK) {
+                    startLocationUpdates(); //Se encendió la localización!!!
+                } else {
+                    Toast.makeText(this,
+                            "Sin acceso a localización, hardware deshabilitado!",
+                            Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private void startLocationUpdates() {
+        Log.i("LOCATION","1111111111111111111");
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+            Log.i("LOCATION","222222222222222222");
+        }
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+
+
+
+
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_PERMISSION: {
+                //LocationView();
+                break;
+            }
+        }
     }
 
 
@@ -39,8 +242,12 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng bogota = new LatLng(4.597908 , -74.076066);
+        //LatLng bogota2 = new LatLng(4.397908 , -74.076066);
+        //mMap.addMarker(new MarkerOptions().position(bogota).title("Marcador en Plaza de Bolívar").snippet("test").alpha(1f).icon(BitmapDescriptorFactory.fromResource(R.drawable.placeholder)));
+        //mMap.addMarker(new MarkerOptions().position(bogota2).title("Marcador en Plaza de Bolívar").snippet("test").alpha(1f).icon(BitmapDescriptorFactory.fromResource(R.drawable.userpin)));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bogota));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
     }
 }
